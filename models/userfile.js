2afb3e18148d4730d57ec392ec95c17029706318
Petrut Jianu <c.petrut97@gmail.com>
2020-10-27 14:26:12 +0100

    Modify file schema

--- models/userfile.js
+++ models/userfile.js
@@ -4,7 +4,11 @@ const UserFileSchema = mongoose.Schema({
   file_id: mongoose.Schema.Types.ObjectId,
   owner_id: String,
   content: String,
-  pub_date: Date
+  location : String,
+  filetype : String,
+  created_date : Date,
+  size : Number,
+  modified_date : Date
 });
 
 module.exports = mongoose.model("UserFile", UserFileSchema);
